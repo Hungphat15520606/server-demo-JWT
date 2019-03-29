a7706c964746bb73c39d8a01d1e90490a8af4872
Hung Phat <phatth2016@gmail.com>
2019-03-29 21:32:21 +0700

    make it better

--- middleware.js
+++ middleware.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
 const secret = 'mysecretsshhh';
+
 const withAuth = function(req, res, next) {
-  const token =
-    req.body.token ||
-    req.query.token ||
-    req.headers['x-access-token'] ||
-    req.cookies.token;
+  const token = 
+      req.body.token ||
+      req.query.token ||
+      req.headers['x-access-token'] ||
+      req.cookies.token;
+
   if (!token) {
     res.status(401).send('Unauthorized: No token provided');
   } else {
@@ -19,4 +21,5 @@ const withAuth = function(req, res, next) {
     });
   }
 }
+
 module.exports = withAuth;
